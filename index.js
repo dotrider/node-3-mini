d0db175db3ab030340f8c5630380581e5f466197
dotrider <56666461+dotrider@users.noreply.github.com>
2020-03-17 18:38:52 -0700

    completed

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
 const express = require("express");
-
+const massive = require('massive')
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STR} = process.env;
+const controller = require('./controller');
 
 app.use(express.json());
+
+massive(CONNECTION_STR).then(db => {
+  app.set('db',db);
+  console.log('Connected to DB')
+  // db.new_planes().then(planes => console.log(planes))
+  // .catch(err => console.log(err))
+
+  // db.get_planes().then( planes => console.log( planes))
+  // .catch(err => console.log(err))
+})
+
+
+app.get(`/api/planes`, controller.getPlanes);
+
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
